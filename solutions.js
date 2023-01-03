577bbdd36f10ead2e5664853087b6a53802e0840
Abhi123sheks <Abhishek47619>
2023-01-03 21:28:43 +0530

    showmyproblem

---
+++ solutions.js
@@ -0,0 +1,27 @@
+let N=13;
+let factor=0;
+for(let i=0; i<=N; i++){
+	if(N%i==0){
+	f++;
+}
+}
+if(f==2){
+console.log(prime)
+}
+else{
+console.log(not prime)
+}
+
+
+// 2 Number
+let n=level;
+let bag="";
+for(let i=n.length-1; i>=0; i--){
+	bag=bag+n[i]
+}
+if(bag==str){
+console.log(Its palindrone))
+}
+else{
+console.log(Its not palindrone);
+0 comments on commit 1e7f9ab
