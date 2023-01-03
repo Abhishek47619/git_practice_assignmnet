e005947fcc0d64de18bc725c5fa58b4bb14cd1eb
Rishabh <rishabhsrivastav722@gmail.com>
2023-01-03 21:42:05 +0530

    yourproblemissolved

---
+++ problem1.js
@@ -0,0 +1,26 @@
+let N=13;
+let factor=0;
+for(let i=0; i<=N; i++){
+	if(N%i==0){
+	factor++;
+}
+}
+if(factor==2){
+console.log(prime)
+}
+else{
+console.log(not prime)
+}
+
+
+// 2 Number
+let n=level;
+let bag="";
+for(let i=n.length-1; i>=0; i--){
+	bag=bag+n[i]
+}
+if(bag==str){
+console.log(Its palindrone))
+}
+else{
+console.log(Its not palindrone);
